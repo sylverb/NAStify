7c6b0d68be992f1329ff409700f7e6ff8508a0f8
Sylver Bruneau <sylver.bruneau@gmail.com>
2015-12-14 21:47:42 +0100

    UPnP plugin : fix 100 items limit on some servers

--- NAStify/ConnectionManager/Plugins/UPnP/CMUPnP.m
+++ NAStify/ConnectionManager/Plugins/UPnP/CMUPnP.m
@@ -40,6 +40,7 @@
 {
     NSString *sortCriteria = @"";
     NSMutableString *outSortCaps = [[NSMutableString alloc] init];
+    NSInteger receivedNumber = 0;
 
     if (!_UPNPdevice)
     {
@@ -59,88 +60,102 @@
     NSMutableString *outNumberReturned = [[NSMutableString alloc] init];
     NSMutableString *outTotalMatches = [[NSMutableString alloc] init];
     NSMutableString *outUpdateID = [[NSMutableString alloc] init];
-    
-    [[_UPNPdevice contentDirectory] BrowseWithObjectID:_UPNProotID BrowseFlag:@"BrowseDirectChildren" Filter:@"*" StartingIndex:@"0" RequestedCount:@"10000" SortCriteria:sortCriteria OutResult:outResult OutNumberReturned:outNumberReturned OutTotalMatches:outTotalMatches OutUpdateID:outUpdateID];
-    
-    [_mutableObjectList removeAllObjects];
-    NSData *didl = [outResult dataUsingEncoding:NSUTF8StringEncoding];
-    MediaServerBasicObjectParser *parser = [[MediaServerBasicObjectParser alloc] initWithMediaObjectArray:_mutableObjectList itemsOnly:NO];
-    [parser parseFromData:didl];
 
-    NSMutableArray *filesOutputArray = [NSMutableArray arrayWithCapacity:[_mutableObjectList count]];
-    // To convert ISO8601 date to NSDate
-    ISO8601DateFormatter *dateFormatter = [[ISO8601DateFormatter alloc] init];
+    NSMutableArray *filesOutputArray = [NSMutableArray array];
 
-    for (MediaServer1BasicObject *item in _mutableObjectList)
-    {
+    do {
+        [[_UPNPdevice contentDirectory] BrowseWithObjectID:_UPNProotID
+                                                BrowseFlag:@"BrowseDirectChildren"
+                                                    Filter:@"*"
+                                             StartingIndex:[@(receivedNumber) stringValue]
+                                            RequestedCount:@"0"
+                                              SortCriteria:sortCriteria
+                                                 OutResult:outResult
+                                         OutNumberReturned:outNumberReturned
+                                           OutTotalMatches:outTotalMatches
+                                               OutUpdateID:outUpdateID];
+        
+        receivedNumber += [outNumberReturned integerValue];
+        [_mutableObjectList removeAllObjects];
+        
+        NSData *didl = [outResult dataUsingEncoding:NSUTF8StringEncoding];
+        MediaServerBasicObjectParser *parser = [[MediaServerBasicObjectParser alloc] initWithMediaObjectArray:_mutableObjectList itemsOnly:NO];
+        [parser parseFromData:didl];
+        
+        // To convert ISO8601 date to NSDate
+        ISO8601DateFormatter *dateFormatter = [[ISO8601DateFormatter alloc] init];
+        
+        for (MediaServer1BasicObject *item in _mutableObjectList)
         {
-            if ([item isContainer])
             {
-                MediaServer1ContainerObject *containerItem = (MediaServer1ContainerObject *)item;
-                NSDictionary *dictItem = [NSDictionary dictionaryWithObjectsAndKeys:
-                                          @"1",@"isdir",
-                                          [containerItem title],@"filename",
-                                          [containerItem objectID],@"id",
-                                          @"dir",@"type",
-                                          nil];
-                [filesOutputArray addObject:dictItem];
-            }
-            else
-            {
-                MediaServer1ItemObject *mediaItem = (MediaServer1ItemObject *)item;
-                
-                NSString *fileType = [mediaItem.uri pathExtension];
-                NSString *name = nil;
-                
-                // filter out parameters in extension
-                NSRange position = [fileType rangeOfString:@"?"];
-                if (position.location != NSNotFound)
+                if ([item isContainer])
                 {
-                    fileType = [fileType substringToIndex:position.location];
-                }
-                if (fileType.length > 0)
-                {
-                    name = [NSString stringWithFormat:@"%@.%@",[mediaItem title],fileType];
+                    MediaServer1ContainerObject *containerItem = (MediaServer1ContainerObject *)item;
+                    NSDictionary *dictItem = [NSDictionary dictionaryWithObjectsAndKeys:
+                                              @"1",@"isdir",
+                                              [containerItem title],@"filename",
+                                              [containerItem objectID],@"id",
+                                              @"dir",@"type",
+                                              nil];
+                    [filesOutputArray addObject:dictItem];
                 }
                 else
                 {
-                    name = [mediaItem title];
+                    MediaServer1ItemObject *mediaItem = (MediaServer1ItemObject *)item;
+                    
+                    NSString *fileType = [mediaItem.uri pathExtension];
+                    NSString *name = nil;
+                    
+                    // filter out parameters in extension
+                    NSRange position = [fileType rangeOfString:@"?"];
+                    if (position.location != NSNotFound)
+                    {
+                        fileType = [fileType substringToIndex:position.location];
+                    }
+                    if (fileType.length > 0)
+                    {
+                        name = [NSString stringWithFormat:@"%@.%@",[mediaItem title],fileType];
+                    }
+                    else
+                    {
+                        name = [mediaItem title];
+                    }
+                    
+                    if ([mediaItem.uri hasPrefix:@"rtsp://"])
+                    {
+                        fileType = @"rtsp";
+                    }
+                    else if (fileType.length == 0)
+                    {
+                        fileType = @"avi";
+                    }
+                    
+                    // Build entry
+                    NSMutableDictionary *dictItem= [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                                    @"0",@"isdir",
+                                                    name,@"filename",
+                                                    [mediaItem uriCollection],@"id",
+                                                    fileType,@"type",
+                                                    nil];
+                    if ([mediaItem.size longLongValue] != 0)
+                    {
+                        [dictItem addEntriesFromDictionary:[NSDictionary dictionaryWithObject:[NSNumber numberWithLongLong:[mediaItem.size longLongValue]]
+                                                                                       forKey:@"filesizenumber"]];
+                    }
+                    
+                    if (mediaItem.date)
+                    {
+                        NSDate *date = [dateFormatter dateFromString:mediaItem.date];
+                        
+                        [dictItem addEntriesFromDictionary:[NSDictionary dictionaryWithObject:[NSNumber numberWithLongLong:[date timeIntervalSince1970]]
+                                                                                       forKey:@"date"]];
+                    }
+                    
+                    [filesOutputArray addObject:dictItem];
                 }
-                
-                if ([mediaItem.uri hasPrefix:@"rtsp://"])
-                {
-                    fileType = @"rtsp";
-                }
-                else if (fileType.length == 0)
-                {
-                    fileType = @"avi";
-                }
-                
-                // Build entry
-                NSMutableDictionary *dictItem= [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                                @"0",@"isdir",
-                                                name,@"filename",
-                                                [mediaItem uriCollection],@"id",
-                                                fileType,@"type",
-                                                nil];
-                if ([mediaItem.size longLongValue] != 0)
-                {
-                    [dictItem addEntriesFromDictionary:[NSDictionary dictionaryWithObject:[NSNumber numberWithLongLong:[mediaItem.size longLongValue]]
-                                                                                   forKey:@"filesizenumber"]];
-                }
-                
-                if (mediaItem.date)
-                {
-                    NSDate *date = [dateFormatter dateFromString:mediaItem.date];
-
-                    [dictItem addEntriesFromDictionary:[NSDictionary dictionaryWithObject:[NSNumber numberWithLongLong:[date timeIntervalSince1970]]
-                                                                                   forKey:@"date"]];
-                }
-                
-                [filesOutputArray addObject:dictItem];
             }
         }
-    }
+    } while ((receivedNumber < [outTotalMatches integerValue]) && ([outNumberReturned integerValue] > 0));
     
     dispatch_async(dispatch_get_main_queue(), ^{
         [self.delegate CMFilesList:[NSDictionary dictionaryWithObjectsAndKeys:
